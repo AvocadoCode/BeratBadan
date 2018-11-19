5dd9cdd3e77b1c9e2f109d404092cf5a4cc2bb69
AvocadoCode <43976112+AvocadoCode@users.noreply.github.com>
2018-11-19 14:33:32 +0700

    Initial commit

---
+++ app/src/main/java/com/idn/avocadocode/beratbadan/TentangActivity.java
@@ -0,0 +1,13 @@
+package com.idn.avocadocode.beratbadan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TentangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang);
+    }
+}
